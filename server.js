80995a1e7cbdc9531ac11adfd9dd0416d33ff18e
Taniya Singh <singhtaniya814@gmail.com>
2026-03-15 21:50:14 +0530

    removed duplicate contact route

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const contactRoutes = require("./Routes/contactRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://singhtaniya814_db_user:Taniya%4052570@cluster0.zpmh4t7.mongodb.net/risingacademy?retryWrites=true&w=majority");
+/* MongoDB Connection */
+
+mongoose.connect(
+"mongodb+srv://singhtaniya814_db_user:Taniya%4052570@cluster0.zpmh4t7.mongodb.net/risingacademy?retryWrites=true&w=majority"
+)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+/* Routes */
 
 app.use("/contact", contactRoutes);
 
+/* Server */
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+
+app.listen(PORT, () => {
+console.log("Server running on port " + PORT);
+});
